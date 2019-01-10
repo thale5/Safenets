168e873d57ba260b789a522c1c08643993fe516d
thale5 <thale5@users.noreply.github.com>
2019-01-10 20:03:09 +0200

    Created.

---
+++ Safenets/Mod.cs
@@ -0,0 +1,29 @@
+﻿using ICities;
+
+namespace Safenets
+{
+    public sealed class Mod : IUserMod, ILoadingExtension
+    {
+        static bool created = false;
+        public string Name => "Safenets";
+        public string Description => "Fix for RoadBaseAI.CanEnableTrafficLights";
+
+        public void OnLevelLoaded(LoadMode mode) { }
+        public void OnLevelUnloading() { }
+
+        public void OnCreated(ILoading loading)
+        {
+            if (!created)
+            {
+                Fixes.Create().Deploy();
+                created = true;
+            }
+        }
+
+        public void OnReleased()
+        {
+            Fixes.instance?.Dispose();
+            created = false;
+        }
+    }
+}
---
+++ Safenets/Types.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Safenets
+{
+    public abstract class Instance<T>
+    {
+        private static T inst;
+
+        public static T instance
+        {
+            get => inst;
+            set => inst = value;
+        }
+
+        public static bool HasInstance => inst != null;
+
+        internal static T Create()
+        {
+            if (inst == null)
+                inst = (T) Activator.CreateInstance(typeof(T), true);
+
+            return inst;
+        }
+    }
+}
