03053c78a722a200c66981aaca4571c4f6896336
thale5 <thale5@users.noreply.github.com>
2024-09-16 21:20:34 +0300

    Inactivated

--- Safenets/Mod.cs
+++ Safenets/Mod.cs
@@ -2,28 +2,9 @@
 
 namespace Safenets
 {
-    public sealed class Mod : IUserMod, ILoadingExtension
+    public sealed class Mod : IUserMod
     {
-        static bool created = false;
         public string Name => "Safenets";
-        public string Description => "Fix for RoadBaseAI.CanEnableTrafficLights";
-
-        public void OnLevelLoaded(LoadMode mode) { }
-        public void OnLevelUnloading() { }
-
-        public void OnCreated(ILoading loading)
-        {
-            if (!created)
-            {
-                Fixes.Create().Deploy();
-                created = true;
-            }
-        }
-
-        public void OnReleased()
-        {
-            Fixes.instance?.Dispose();
-            created = false;
-        }
+        public string Description => "Mod is inactive, no longer needed";
     }
 }
